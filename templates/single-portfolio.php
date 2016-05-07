32e26d6368905e6d5644827c1cbf2058ae64dc06
ihsanatkia <ihsan.job@gmail.com>
2016-05-07 07:49:32 +0700

    style single new

--- templates/single-portfolio.php
+++ templates/single-portfolio.php
@@ -2,29 +2,24 @@
 
 get_header(); 
 global $post; ?>
-
-<div id="content">
+	
 	<div class="container">
 		<h1 class="title-single"><?php the_title(); ?></h1>
-		<div class="main-shot ">
-			<div class="the-shot" data-screenshot_id="2194714" data-img-src="https://d13yacurqjgara.cloudfront.net/users/360131/screenshots/2194714/gif-testtube-good.gif">
-				<div class="single group">
-					<div class="single-grid">
-						<div class="single-img">
-							<?php the_post_thumbnail(); ?>
-						</div>
+		<div class="main-portfolio ">
+			<div class="left-content">
+				<figure>
+					<div class="single-img">
+						<?php the_post_thumbnail(); ?>
 					</div>
-				</div>
+				</figure>
 			</div>
 
-			<div class="screenshot-info-wrapper">
-				<div class="screenshot-conversation">
-					<div class="shot-desc">
-						<?php echo $post->post_content; ?>
-					</div>
+			<div class="right-content">
+				<div class="shot-desc">
+					<p><?php echo $post->post_content; ?></p>
 				</div>
 
-				<div class="screenshot-meta">
+				<div class="portofolio-meta">
 					<div class="tags-section">
 						<h3 class="meta-head">Category</h3>
 						<ol id="tags" class="popular-tags">
@@ -32,7 +27,7 @@ global $post; ?>
 								$terms = get_the_terms( get_the_ID(), 'portfoliox_category' );
 								if ( ! empty( $terms ) ) {
 									foreach ($terms as $value) {
-										echo "<li class='cat'>" . $value->name . "</li>";
+										echo "<li class='cat'>" . $value->name . " ,</li>";
 									}
 								}
 							?>
@@ -46,7 +41,7 @@ global $post; ?>
 
 								if ( ! empty( $terms ) ) {
 									foreach ($terms as $value) {
-										echo "<li class='cat'>" . $value->name . "</li>";
+										echo "<li class='cat'>" . $value->name . " ,</li>";
 									}
 								}
 							?>
@@ -56,7 +51,6 @@ global $post; ?>
 			</div>
 		</div>
 	</div>
-</div>
 
 <?php
 get_footer();
